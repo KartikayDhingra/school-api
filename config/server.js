9c02f3f7b0bef3d3489e9b81c5a3b85bfa1a24d8
vinayakdhingra19 <amartk00@gmail.com>
2021-07-16 18:27:52 +0530

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae327dc701b3fd39ef6ab8e88620d017'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '494a2542-2cdd-4af0-b8b4-8b55c6acd2db'
+};
